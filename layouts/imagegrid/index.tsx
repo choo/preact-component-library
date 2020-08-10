b7ed9139a3b6a27a65dccf216b1d9b90b6c36d36
choo <sugimori.choo@gmail.com>
2020-08-10 10:12:56 +0900

    Extract imagecell component from imagegrid component

--- layouts/imagegrid/index.tsx
+++ layouts/imagegrid/index.tsx
@@ -1,13 +1,7 @@
 import { h, FunctionalComponent } from 'preact';
-import { useState } from 'preact/hooks';
+import {ImageCell, Image} from '../imagecell'
 import Grid from '../grid'
 
-
-interface Image {
-  path: string,
-  url?: string,
-  secondPath?: string, // path for notFound error
-};
 interface Props {
   images: Image[],
   width?: number,
@@ -22,11 +16,7 @@ const ImageGrid: FunctionalComponent<Props> = (props: Props) => {
       {props.images.length > 0 ? props.images.map((img) => {
         return (
         <Grid m={'0 0 10px'} p={'1px'} style={{textAlign: 'center'}}>
-          <div>
-            <a href={img.url || 'javascript:void(0)'} target={'_blank'}>
-              <ImageCell img={img} width={width} />
-            </a>
-          </div>
+          <ImageCell image={img} width={width} />
         </Grid>
         );
       }) : (
@@ -36,31 +26,5 @@ const ImageGrid: FunctionalComponent<Props> = (props: Props) => {
   );
 };
 
-interface ImageProps {
-  img: Image,
-  width?: number,
-};
-
-const ImageCell: FunctionalComponent<ImageProps> = (props: ImageProps) => {
-  const [errored, setErrored] = useState<boolean>(false);
-  const replacePath = (event: any, path: string) => {
-    if (!errored) {
-      const elm = event.target;
-      elm.onerror = null;
-      elm.src = path;
-      setErrored(true);
-    }
-  };
-  const img = props.img;
-
-  return (
-    <img src={img.path} alt={'Not Found'}
-      style={{width: `${props.width}px`,}}
-      onError={(evt) => {
-        replacePath(evt, img.secondPath || '')
-      }}
-    />
-  );
-};
 
 export default ImageGrid;
