d3f3eef59941361969871c334aa19a3c20118085
choo <choo.hacks@gmail.com>
2020-08-10 17:20:39 +0900

    Add resultlist sample page

---
+++ templates/resultlist/index.tsx
@@ -0,0 +1,97 @@
+import { h, FunctionalComponent } from 'preact';
+
+import {buildParams} from '../../utils/param';
+
+import ResultList from '../../resultlist';
+import Paper from '../../inputs/paper';
+import Grid from '../../layouts/grid';
+import TextField from '../../inputs/textfield'
+
+
+interface Item {
+  [s: string]: string | number | any[],
+};
+interface ResultItem extends Item {
+  name: string,
+  yomi: string,
+    //org: string
+    //count: number,
+}
+type Params = {
+  page?: number,
+  ippg?: number,
+  kwd?: string,
+};
+
+const AJAX_URL = '/assets/search_sample.json';
+
+const MenuList: FunctionalComponent<Params> = (props: Params) => {
+  const initialParams: Params = {
+    kwd: props.kwd || '',
+    page: props.page || 1,
+    ippg: props.ippg || 20,
+  };
+  const params: Params = buildParams(initialParams);
+  return (
+    <ResultList {...props}
+      params={params}
+      renderCassette={(item: ResultItem, idx: number) => {
+        return <Cassette item={item} idx={idx} />
+      }}
+      renderConds={(updateParams: any, params: Params) => {
+        return <Conditions updateParams={updateParams} params={params} />
+      }}
+      ajaxUrl={AJAX_URL}
+    />
+  )
+};
+
+
+type CassetteProps = {
+  item: ResultItem,
+  idx: number,
+};
+const Cassette: FunctionalComponent<CassetteProps> = (props: CassetteProps) => {
+  const item = props.item;
+  return (
+    <Grid container={true} m={'0 0 12px'}>
+      <Grid flex={1}>
+        <Paper style={{padding: '8px'}}>
+          <a href={`/detail/${item.name}`}>
+            <h2 style={{margin: '0'}}>{props.idx}. {item.name}</h2>
+          </a>
+          <span>{item.yomi}</span>
+        </Paper>
+      </Grid>
+    </Grid>
+  )
+};
+
+
+
+type CondProps = {
+  params: Params,
+  updateParams: any,
+};
+const Conditions: FunctionalComponent<CondProps> = (props: CondProps) => {
+  return (
+    <Grid container m='4px 0 24px' p='0'>
+      <Grid flex={1}>
+        <TextField
+          label='food name'
+          onKeyDown={(e) => {
+            if (e.keyCode === 13) {
+              const kwd = e.currentTarget.value;
+              if (props.params.kwd !== kwd) {
+                props.updateParams({kwd: kwd});
+                //e.target.blur();
+              }
+            }
+          }}
+        />
+      </Grid>
+    </Grid>
+  )
+};
+
+export default MenuList;
