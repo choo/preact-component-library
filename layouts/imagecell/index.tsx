5a04149a679bb36cab0755cb7d63a8c237ffbb20
choo <choo.hacks@gmail.com>
2020-08-10 10:12:56 +0900

    Extract imagecell component from imagegrid component

---
+++ layouts/imagecell/index.tsx
@@ -0,0 +1,39 @@
+import { h, FunctionalComponent } from 'preact';
+import { useState } from 'preact/hooks';
+
+
+export interface Image {
+  path: string,
+  url?: string,
+  secondPath?: string, // path for notFound error
+};
+
+interface Props {
+  image: Image,
+  width?: number,
+  style?: {[s: string]: string | number},
+};
+
+export const ImageCell: FunctionalComponent<Props> = (props: Props) => {
+  const [errored, setErrored] = useState<boolean>(false);
+  const replacePath = (event: any, path: string) => {
+    if (!errored) {
+      const elm = event.target;
+      elm.onerror = null;
+      elm.src = path;
+      setErrored(true);
+    }
+  };
+  const img = props.image;
+
+  return (
+    <a href={img.url || 'javascript:void(0)'} target={'_blank'}>
+      <img src={img.path} alt={'Not Found'}
+        style={{width: `${props.width}px`, ...props.style}}
+        onError={(evt) => {
+          replacePath(evt, img.secondPath || '')
+        }}
+      />
+    </a>
+  );
+};
