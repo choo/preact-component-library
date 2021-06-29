dbf09ee1ebe6831e05536dbea28ec75d9613f473
choo <choo.hacks@gmail.com>
2021-06-29 11:04:31 +0900

    Update flip-flop-counter

--- resultlist/animation-counter/index.js
+++ resultlist/animation-counter/index.js
@@ -3,35 +3,36 @@ import { useState, useEffect } from 'preact/hooks';
 /**
  * props:
  *    target   : (required) Target Value to render 
- *    initial  : (required) Initial Value
  *    duration : (optional) 300 (ms)
  *    interval : (optional) 20 (ms)
  *    style : (optional) style settings
  */
 const AnimationCounter = props => {
-  const initial = props.initial;
+  const [prev, setPrev] = useState(0);
   const target  = props.target;
-  const duration = props.duration || 500; // ms
+  const duration = props.duration || 300; // ms
   const interval = props.interval || 20; // ms
-  const isUp   = initial <= target;
-  const [value, setValue] = useState(initial);
+  const [value, setValue] = useState(prev);
 
-  const diff   = target - initial;
-  const steps  = Math.ceil(duration / interval);
-  const delta  = Math.max(1, Math.ceil(diff / steps));
+  const isUp = prev <= target;
+  const diff = Math.abs(target - prev);
+  const numSteps = Math.ceil(duration / interval);
+  const step = Math.max(1, Math.ceil(diff / numSteps));
+  const delta = isUp ? step : -step;
 
   useEffect(() => {
     const timer = setInterval(() => {
       const next = value + delta;
       if (isUp && next < target || !isUp && next > target) {
-        setValue(next)
+        setValue(next);
       } else {
-        setValue(target)
+        setValue(target);
+        setPrev(target);
         clearInterval(timer);
       }
     }, interval);
     return () => clearInterval(timer);
-  }, [value]);
+  });
 
   return (
     <span style={{...props.style}}>
