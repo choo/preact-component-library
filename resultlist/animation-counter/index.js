2391b96756996e9cfa13e28a66e9d29d7c0bde3a
choo <choo.hacks@gmail.com>
2021-07-09 17:22:25 +0900

    Fix a bug that the delta was set to 1 when the target changed while the value was changing

--- resultlist/animation-counter/index.js
+++ resultlist/animation-counter/index.js
@@ -8,14 +8,18 @@ import { useState, useEffect } from 'preact/hooks';
  *    style : (optional) style settings
  */
 const AnimationCounter = props => {
+  const [value, setValue] = useState(0);
   const [prev, setPrev] = useState(0);
-  const target  = props.target;
+  const [target, setTarget] = useState(null);
   const duration = props.duration || 300; // ms
   const interval = props.interval || 20; // ms
-  const [value, setValue] = useState(prev);
+  if (target === null || props.target != target) {
+    setPrev(value);
+  }
+  setTarget(props.target);
 
-  const isUp = prev <= target;
-  const diff = Math.abs(target - prev);
+  const isUp = prev <= props.target;
+  const diff = Math.abs(props.target - prev);
   const numSteps = Math.ceil(duration / interval);
   const step = Math.max(1, Math.ceil(diff / numSteps));
   const delta = isUp ? step : -step;
@@ -26,6 +30,7 @@ const AnimationCounter = props => {
       if (isUp && next < target || !isUp && next > target) {
         setValue(next);
       } else {
+        // completed update
         setValue(target);
         setPrev(target);
         clearInterval(timer);
